86d69cfe65aa20d79d4c4b949b76b2145213c221
Ali Naqvi <57028815+AliNaqvi110@users.noreply.github.com>
2023-08-30 14:30:14 +0500

    Time: 54 ms (17.94%), Space: 16.1 MB (91.77%) - LeetHub

--- 2876-number-of-employees-who-met-the-target/2876-number-of-employees-who-met-the-target.py
+++ 2876-number-of-employees-who-met-the-target/2876-number-of-employees-who-met-the-target.py
@@ -1,7 +1,10 @@
 class Solution:
     def numberOfEmployeesWhoMetTarget(self, hours: List[int], target: int) -> int:
         max_num = max(hours)
+        min_num = min(hours)
         if target > max_num:
             return 0
+        elif (target < min_num):
+            return len(hours)
         else:
             return sum([x >= target for x in hours])
