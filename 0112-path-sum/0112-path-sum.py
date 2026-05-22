6561d0fcebf30f0c1d07638ba5a42898cfdeb931
Ali Naqvi <57028815+AliNaqvi110@users.noreply.github.com>
2026-05-22 18:13:55 +0500

    Time: 0 ms (100%), Space: 20.3 MB (21.6%) - LeetHub

--- 0112-path-sum/0112-path-sum.py
+++ 0112-path-sum/0112-path-sum.py
@@ -9,6 +9,7 @@ class Solution:
         if not root:
             return False
         if not root.left and not root.right:
-            return root.val == targetSum
-        return (self.hasPathSum(root.left, targetSum-root.val) or
-        self.hasPathSum(root.right, targetSum-root.val))
+            return targetSum == root.val
+        remaining = targetSum - root.val
+        
+        return (self.hasPathSum(root.left, remaining) or self.hasPathSum(root.right, remaining))
